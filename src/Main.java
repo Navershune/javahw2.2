4632e7cdde74cc208a6217ba0d6665d216781074
Ilia Firsov <i.fiirsov@gmail.com>
2023-05-04 20:10:37 +0300

    second

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
         int balance = 300;
         int payment = 5000;
         int bonus;
-        if (payment < 1000)
+        if (payment < 1000) {
             bonus = 0;
-        else
+        }
+        else {
             bonus = payment / 100;
+        }
         int total = balance + payment + bonus;
         System.out.println("Balance=" + total);
         System.out.println("Bonus=" + bonus);
